6a6228452dd2be4611009cb79edbcd5ecfdc907a
apriliyaard <3103117254@student.smktelkom-pwt.sch.id>
2020-11-20 11:01:29 +0700

    update tampilan

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,41 @@ $('.center').slick({
         }
     ]
 });
+
+// insta feed
+$(document).ready(function() {
+    $.instagramFeed({
+        'username': 'katalogcressidaofficial',
+        'container': "#instagram-feed",
+        'display_profile': false,
+        'display_biography': false,
+        'display_gallery': true,
+        'get_raw_json': false,
+        'callback': null,
+        'styling': true,
+        'items': 4,
+        'items_per_row': 4,
+        'margin': 0.3,
+    });
+});
+
+// scroll to top
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 200){
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+    $('.scrollToTop').click(function(){
+        $('html,body').animate({scrollTop: 0}, 1000)
+    })
+});
+
+function back() {
+    window.history.back();
+}
+
+var obj = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');
+document.getElementById("demo").innerHTML = obj.name + ", " + obj.age;
